c233808e036d5692dd4407538b1bb2db0634807c
sii-yan <naobeedesign@gmail.com>
2025-04-03 00:37:10 +0900

    feat: アカウント登録、ログイン画面の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'posts/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "posts#index"
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
 end
