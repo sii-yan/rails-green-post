ba32d3875d40216a6739f665c381cb54aabad86b
sii-yan <naobeedesign@gmail.com>
2025-04-03 09:07:02 +0900

    feat: ユーザーアカウントとプロフィール管理機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root "posts#index"
 
+  namespace :users do
+    resource :profile, only: [:show, :edit, :update], controller: 'profile'
+    resource :account, only: :show, controller: 'account'
+  end
+
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
