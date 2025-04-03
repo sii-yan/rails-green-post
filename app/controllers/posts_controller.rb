fc56c6808c65761481feca7594caa8b52861eeb1
sii-yan <naobeedesign@gmail.com>
2025-04-03 23:58:18 +0900

    feat: 新規投稿機能のスタイルの追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,15 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to @post, notice: "投稿しました。"
+    else
+      flash.now[:alert] = "投稿に失敗しました。"
+      render :new
+    end
+  end
 end
