268fbd84ea15ddf1f6230d10e94982dc2d23a2c3
sii-yan <naobeedesign@gmail.com>
2025-04-10 20:34:08 +0900

    fix: comments に dependent: :destroy を追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
 
@@ -9,4 +9,12 @@ class Post < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 30 }
   validates :content, presence: true, length: { maximum: 1000 }
+
+  after_destroy :purge_image
+
+  private
+
+  def purge_image
+    image.purge_later if image.attached?
+  end
 end
